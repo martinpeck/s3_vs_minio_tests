0ad0282b7ef5127c5c5ac60aa7fb23e579386d7e
Martin Peck <mpeck@microsoft.com>
2018-06-22 22:38:38 +0100

    add upload and download s3 tests

---
+++ sample_files/README.md
@@ -0,0 +1,5 @@
+# Test Setup
+
+There are a number of tests that download files from cloud storage.
+
+Before running these tests you need to first upload the contents of this folder into an S3 bucket.
---
+++ tests/test_s3.py
@@ -0,0 +1,29 @@
+import boto3
+import pytest
+import uuid
+import os
+
+aws_upload_bucket = os.environ["AWS_UPLOAD_BUCKET"]
+aws_download_bucket = os.environ["AWS_DOWNLOAD_BUCKET"]
+
+
+def test_uploading_a_text_file():
+    """uploads a text file to an s3 bucket"""
+
+    uploaded_file_name = f"{uuid.uuid4()}.txt"
+
+    s3 = boto3.resource("s3")
+    s3.meta.client.upload_file(
+        "sample_files/hello.txt", aws_upload_bucket, uploaded_file_name
+    )
+
+
+def test_downloading_a_file():
+    """downloads a text file from an s3 bucket"""
+
+    downloaded_file_name = f"{uuid.uuid4()}.txt"
+
+    s3 = boto3.resource("s3")
+    s3.meta.client.download_file(
+        aws_download_bucket, "hello.txt", f"test_temp/{downloaded_file_name}"
+    )
