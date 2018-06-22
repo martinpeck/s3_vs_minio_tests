9edcef1a504cdcba5f25d9c112005d08f4a5f06d
Martin Peck <mpeck@microsoft.com>
2018-06-22 21:09:20 +0100

    add docstrings to my basic tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -2,9 +2,11 @@ import pytest
 
 
 def test_true():
+    """prove that pytest is working"""
     assert True
 
 
 @pytest.mark.xfail
 def test_false():
+    """prove that pytest is working"""
     assert False
